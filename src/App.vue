ee50e0d00326d722ea45e885d37e4164338842d9
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-09 10:50:27 +0700

    fix: strict equal in App.vue

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
   name: 'app',
   methods: {
     toggleSideNavLeft (route) {
-      if (typeof (route) == 'object') {
+      if (typeof (route) === 'object') {
         this.$refs.leftSidenav.toggle()
       } else {
         this.$refs.leftSidenav.toggle()
